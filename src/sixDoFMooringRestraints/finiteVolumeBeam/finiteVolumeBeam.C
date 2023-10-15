7f99a0646d04a5725faeeed05b1595bce7a7223f
Amirhossein Taran <det.em.tw@gmail.com>
2023-10-15 21:57:21 +0100

    moorFV compatible with beamFoam on ESI, just a draft

---
+++ src/sixDoFMooringRestraints/finiteVolumeBeam/finiteVolumeBeam.C
@@ -0,0 +1,134 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2019-2020 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+#include "finiteVolumeBeam.H"
+#include "addToRunTimeSelectionTable.H"
+#include "sixDoFRigidBodyMotion.H"
+#include "Time.H"
+#include "fvMesh.H"
+#include "OFstream.H"
+#include "quaternion.H"
+
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace sixDoFRigidBodyMotionRestraints
+{
+    defineTypeNameAndDebug(finiteVolumeBeam, 0);
+
+    addToRunTimeSelectionTable
+    (
+        sixDoFRigidBodyMotionRestraint,
+        finiteVolumeBeam,
+        dictionary
+    );
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::sixDoFRigidBodyMotionRestraints::finiteVolumeBeam::finiteVolumeBeam
+(
+    const word& name,
+    const dictionary& sDoFRBMRDict
+    //const Time& time
+)
+:
+    sixDoFRigidBodyMotionRestraint(name, sDoFRBMRDict),
+    //beam_(beamModel::New(const_cast<Time&>(time), Foam::dynamicFvMesh::defaultRegion)),
+    refAttachmentPt_()
+{
+    read(sDoFRBMRDict);
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::sixDoFRigidBodyMotionRestraints::finiteVolumeBeam::~finiteVolumeBeam()
+{}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+
+void Foam::sixDoFRigidBodyMotionRestraints::finiteVolumeBeam::restrain
+(
+    const sixDoFRigidBodyMotion& motion,
+    vector& restraintPosition,
+    vector& restraintForce,
+    vector& restraintMoment
+) const
+{
+   // We need to update this function to call the FV beam solver
+
+    // Calculate new attachment point position
+    //restraintPosition = motion.transform(refAttachmentPt_);
+
+    // Update the beam solver boundary conditions: set the displacement on the
+    // attachment point boundary condition
+    // Lookup the W or DW (displacement) field and then find the attachment end
+    // boundary condition. Then update the displacement on this boundary
+    // condition.
+
+    // Solve the beam equation
+    // beam.evolve();
+
+    // Extract the beam end forces and moments
+
+
+
+
+
+
+
+
+    restraintPosition = motion.transform(refAttachmentPt_);
+}
+
+
+bool Foam::sixDoFRigidBodyMotionRestraints::finiteVolumeBeam::read
+(
+    const dictionary& sDoFRBMRDict
+)
+{
+    sixDoFRigidBodyMotionRestraint::read(sDoFRBMRDict);
+
+    return true;
+}
+
+
+void Foam::sixDoFRigidBodyMotionRestraints::finiteVolumeBeam::write
+(
+    Ostream& os
+) const
+{
+}
+
+
+// ************************************************************************* //
